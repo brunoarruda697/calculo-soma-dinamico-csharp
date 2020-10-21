64337febc448ad0771ae3488df489625c2f08d5b
Bruno Costa <brunoarruda697@gmail.com>
2020-10-21 11:04:54 -0300

    Calculo de soma dinâmico

---
+++ Course/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos números inteiros você vai digitar ? ");
+            int numbers = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for (int count = 1; count <= numbers; count++)
+            {
+                Console.Write("Valor #{0}: ", count);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+}
